8f0deddcc3ce89ac75edb9bcc191206dd3a27dbb
Hunter Parcells <hparcells@icloud.com>
2018-10-23 16:19:50 -0400

    Added the Old Some Web Clicker

---
+++ old/soundHandler.js
@@ -0,0 +1,13 @@
+function click() {
+    var click = new Audio("files/sounds/click.mp3");
+    click.play();
+
+    click.currentTime=0;
+}
+
+function buy() {
+    var buy = new Audio("files/sounds/buy.wav");
+    buy.play();
+
+    buy.currentTime=0;
+}
